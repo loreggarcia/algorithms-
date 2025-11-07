e0e5748cafd744547d961db66c599c5b0421507c
Lorena Gabriela <lorena.garcia@sou.inteli.edu.br>
2025-11-07 03:56:19 -0300

    remove a comments

--- algorithms/search/linearSearch.c
+++ algorithms/search/linearSearch.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-//array 
-
-//sizeof() => returns the size of a type in bytes.
-//each elemente in dataset one by one 
-//create a function
 int numbers[] = {100, 425, 35, 78, 17, 9};
 int length = sizeof(numbers) / sizeof(numbers[0]);
 int i;
