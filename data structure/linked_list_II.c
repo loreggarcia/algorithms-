8db3f57ad9405461b8ae20432ef5c0eb8c9533a9
loreggarcia <lorena.garcia@sou.inteli.edu.br>
2025-11-28 00:47:23 -0300

    Add linked list implementation with print and create node functions

---
+++ data structure/linked_list_II.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node { 
+    int value; 
+    struct node* next; 
+};
+
+typedef struct node node_t; 
+
+void printlist(node_t * head){
+
+    node_t *temporary = head; 
+
+    while (temporary != NULL) { 
+        printf("%d -", temporary->value); 
+        temporary = temporary->next; 
+    }
+
+    printf("\n");
+
+}
+
+node_t *create_new_node(int value){
+    node_t *result = malloc(sizeof(node_t));
+    result->value = value; 
+    result->next = NULL; 
+    return result;
+}
+
+int main(){
+    node_t *head;
+    node_t *tmp;
+
+    head = NULL;
+
+    for (int i=0; i < 25; i++){
+        tmp = create_new_node(i);
+        tmp->next = head;
+        head = tmp;
+    }
+
+    printlist(head);
+    return 0;
+}
