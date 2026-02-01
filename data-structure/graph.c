ad0be3c881bdd58379f9d306b787156f14685af7
loreggarcia <lorena.garcia@sou.inteli.edu.br>
2026-02-01 00:09:23 +0000

    dev: Implement DFS on Adjacency List

--- data-structure/graph.c
+++ data-structure/graph.c
@@ -22,16 +22,29 @@ void BFS(int G[][7], int start, int n){
     printf("\n");
 }
 
+void DFS(int G[][7], int start, int n){
+    static int visited[7]={0};
+    if(visited[start]==0){
+        printf("\n%d\n", start);
+        visited[start]=1;
+        for(int j=1;j<n;j++){
+            if(G[start][j]==1 && visited[j]==0){
+                DFS(G, j, n);
+            }
+        }
+    }
+}
+
 int main(){
     int G[7][7]={{0,0,0,0,0,0,0},
                  {0,0,1,1,0,0,0},
                  {0,1,0,0,1,0,0},
-                 {0,1,0,0,0,1,0},
+                 {0,1,0,0,1,0,0},
                  {0,0,1,1,0,1,1},
                  {0,0,0,0,1,0,0},
                  {0,0,0,0,1,0,0},
                 };
-    BFS(G,1,7);
+    DFS(G,1,7);
 
     return 0;
 }
